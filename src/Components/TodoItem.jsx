fbf31de632b88cf9a9ac3873c2118cd8290130d5
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-02-19 11:12:19 +0500

    New Commit

--- src/Components/TodoItem.jsx
+++ src/Components/TodoItem.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import { DeleteFilled, SignatureFilled } from '@ant-design/icons'
 import { Button } from 'antd'
+import { useDispatch } from 'react-redux'
+import { ACTIONS } from '../redux/actions'
 const TodoItem = ({item, index}) => {
+    const dispatch = useDispatch()
   return (
     <li className='flex justify-between w-full  bg-slate-300 mt-2 p-2 rounded-xl'>
       <div className='flex items-center gap-2'>
@@ -10,7 +13,7 @@ const TodoItem = ({item, index}) => {
       </div>
       <div className='flex items-center gap-2'>
         <Button size='large' type='primary' className='!text-[20px]'><SignatureFilled/></Button>
-        <Button  size='large' type='primary' className='!text-[20px] !bg-red-500'><DeleteFilled/></Button>
+        <Button onClick={()=> dispatch({type:ACTIONS.DELETE_TODO, payload:index})} size='large' type='primary' className='!text-[20px] !bg-red-500'><DeleteFilled/></Button>
       </div>
     </li>
   )
